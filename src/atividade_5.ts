d844b238bff8bf26c8f0d87b43b79188af282ab6
MateusCamargoS-1 <mateuscamargodasilva2@gmail.com>
2024-05-30 20:26:39 -0300

    ajeitando as coisas

--- src/atividade_5.ts
+++ src/atividade_5.ts
@@ -4,49 +4,12 @@ class Desconto {
   recuperarValorTotal() {}
 }
 
-class ValorPedido {
-  itens: ItemPedido[];
-
-  constructor() {
-    this.itens = [];
-  }
-
-  aplicarDescontoEmReais(desconto: number) {}
-
-  removeItem(item: string): void {
-    const index = this.itens.findIndex((i) => i.nome === item);
-    if (index > -1) {
-      this.itens.splice(index, 1);
-    }
-  }
-}
-
-class Pedido extends ValorPedido {
-  itens: ItemPedido[] = [];
-
-  add(item: ItemPedido): void {
-    this.itens.push(item);
-  }
-
-  recuperarValorTotal(): number {
-    let total = this.itens.map((i) => i.recuperarValorTotal()).reduce((sum, v) => sum + v, 0);
-
-    return total;
-  }
-
-  aplicarDescontoEmPorcentagem(desconto: number) {
-    const porcentagem = desconto / 100;
-    const descontoEmReais = this.aplicarDescontoEmReais(desconto);
-  }
-}
-
-class ItemPedido extends ValorPedido {
+class ItemPedido {
   valor: number;
   nome: string;
   quantidade: number;
 
   constructor(valor: number, nome: string, quantidade: number) {
-    super();
     this.valor = valor;
     this.nome = nome;
     this.quantidade = quantidade;
@@ -56,7 +19,49 @@ class ItemPedido extends ValorPedido {
     return this.valor * this.quantidade;
   }
 
-  aplicarDescontoEmReais(desconto: number): void {
+  aplicarDescontoEmReais(desconto: number) {
     this.valor -= desconto;
+  }
+}
+
+class ValorPedido {
+  itens: ItemPedido[];
+
+  constructor() {
+    this.itens = [];
+  }
+
+  aplicarDescontoEmReais(desconto: number) {}
+
+  removeItem(item: string) {
+    const index = this.itens.findIndex((i) => i.nome === item);
+    if (index > -1) {
+      this.itens.splice(index, 1);
+    }
+  }
+}
+
+class Pedido extends ValorPedido {
+  constructor() {
+    super();
+  }
+
+  add(item: ItemPedido): void {
+    this.itens.push(item);
+  }
+
+  recuperarValorTotal(): number {
+    const total = this.itens
+      .map((i) => i.recuperarValorTotal())
+      .reduce((sum, v) => sum + v, 0);
+    return total;
+  }
+
+  aplicarDescontoEmPorcentagem(desconto: number): void {
+    const porcentagem = desconto / 100;
+    this.itens.forEach((item) => {
+      const descontoEmReais = item.valor * porcentagem;
+      item.aplicarDescontoEmReais(descontoEmReais);
+    });
   }
 }
